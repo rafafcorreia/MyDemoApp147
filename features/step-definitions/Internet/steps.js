0f937885e431d2d33edc09d3ca49f44cf7b7f159
rafafcorreia <rafa.f.correia@gmail.com>
2026-07-01 15:47:42 -0300

    test: implemented a step to close the initial chrome popup

--- features/step-definitions/Internet/steps.js
+++ features/step-definitions/Internet/steps.js
@@ -1,23 +1,42 @@
-const { Given, When, Then } = require('@wdio/cucumber-framework');
-const { expect, $ } = require('@wdio/globals')
+const { Given, When, Then, BeforeAll } = require("@wdio/cucumber-framework");
+const { browser, expect, $ } = require("@wdio/globals");
 
-const LoginPage = require('../pageobjects/login.page');
-const SecurePage = require('../pageobjects/secure.page');
+const LoginPage = require("../../pageobjects/Internet/login.page");
+const SecurePage = require("../../pageobjects/Internet/secure.page");
 
 const pages = {
-    login: LoginPage
-}
+  login: LoginPage,
+};
+
+BeforeAll(async () => {
+  const originalContext = await browser.getContext();
+
+  await browser.switchContext("NATIVE_APP");
+
+  const noThanks = await $('android=new UiSelector().text("No thanks")');
+
+  const isNoThanksDisplayed = await noThanks
+    .waitForDisplayed({ timeout: 5000 })
+    .catch(() => null);
+
+  if (isNoThanksDisplayed) {
+    await noThanks.click();
+  }
+
+  await browser.switchContext(originalContext);
+});
 
 Given(/^I am on the (\w+) page$/, async (page) => {
-    await pages[page].open()
+  await pages[page].open();
 });
 
 When(/^I login with (\w+) and (.+)$/, async (username, password) => {
-    await LoginPage.login(username, password)
+  await LoginPage.login(username, password);
 });
 
 Then(/^I should see a flash message saying (.*)$/, async (message) => {
-    await expect(SecurePage.flashAlert).toBeExisting();
-    await expect(SecurePage.flashAlert).toHaveText(expect.stringContaining(message));
+  await expect(SecurePage.flashAlert).toBeExisting();
+  await expect(SecurePage.flashAlert).toHaveText(
+    expect.stringContaining(message),
+  );
 });
-
